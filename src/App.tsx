302e158cf46e6cfaa6ba8c8910ae159e01b26b87
Kritkhachon <kritkhachon.vani@gmail.com>
2026-08-28 08:33:47 +0700

    feat: add ShapeGrid component for dynamic background visuals in App

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,15 @@ import { Footer } from './components/Footer'
 import { Hero } from './components/Hero'
 import { Nav } from './components/Nav'
 import { Projects } from './components/Projects'
+import { ShapeGrid } from './components/ShapeGrid'
 import { Skills } from './components/Skills'
 
 function App() {
   return (
     <>
+      <div className="fixed inset-0 -z-10" aria-hidden="true">
+        <ShapeGrid direction="diagonal" speed={0.4} squareSize={44} hoverTrailAmount={8} />
+      </div>
       <Nav />
       <main className="flex flex-col">
         <Hero />
